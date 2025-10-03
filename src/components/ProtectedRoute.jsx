7657d38bdf17b0e5ceccae9b2177f19c3d305eff
Nikita Markin <mr.nik2014@gmail.com>
2025-10-03 18:03:32 +0300

    FIX: rename AuthContext

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import { useAuth } from '../contexts/auth-context';
+import { useAuth } from '../contexts/AuthContext';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
