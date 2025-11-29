0ed75e0b2c0984672c5ef1034bddfc69c7a3e5a4
Nikita Markin <mr.nik2014@gmail.com>
2025-11-29 13:34:29 +0300

    Redesign sidebar + clean old styles

--- src/pages/app/components/Sidebar.jsx
+++ src/pages/app/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { useNavigation } from '/app/src/hooks/useNavigation';
 import { Dropdown, Space, Avatar, Menu, Select } from 'antd';
 import { UserOutlined, SettingOutlined, LogoutOutlined, BellOutlined, DownOutlined } from '@ant-design/icons';
 import { UserCircleIcon, ChevronDownIcon, UserIcon } from '@heroicons/react/16/solid'
-import '../styles/Sidebar.css';
+import '../styles/Sidebar.scss';
 
 const Sidebar = () => {
   const { activeSection, setActiveSection, openedItems, openItem, closeItem } = useNavigation();
@@ -20,36 +20,39 @@ const Sidebar = () => {
     if (!items?.length) return null;
 
     return items.map(item => (
-      <div key={`${section}-${item.id}`} className="mb-1">
+      <div key={`${section}-${item.id}`} className="section-group">
         <SidebarItem
           item={item}
           onClick={() => openItem(item, item.type)}
           onClose={() => closeItem(item.id, item.type)}
           activeSection={activeSection}
+          isParent={true}
         />
-        
-        {item.openedAssets?.map(asset => (
-          <SidebarItem
-            key={`${asset.type}-${asset.id}`}
-            item={asset}
-            onClick={() => openItem(asset, asset.type, item.id)}
-            onClose={() => closeItem(asset.id, asset.type, item.id)}
-            activeSection={activeSection}
-          />
-        ))}
+
+        <div className="group-children-list">
+          {item.openedAssets?.map(asset => (
+            <SidebarItem
+              key={`${asset.type}-${asset.id}`}
+              item={asset}
+              onClick={() => openItem(asset, asset.type, item.id)}
+              onClose={() => closeItem(asset.id, asset.type, item.id)}
+              activeSection={activeSection}
+            />
+          ))}
+        </div>
       </div>
     ));
   };
 
   return (
-    <div className="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary sidebar" style={{ width: '250px' }}>
+    <div id="sidebar">
       <SidebarHeader />
       
-      <nav className="nav nav-pills flex-column mb-auto">
+      <nav className="menu">
         {menuItems.map(({ id, label, icon }) => (
-          <div key={id} className="nav-item">
+          <div key={id} className="menu-section">
             <button
-              className={`nav-link w-100 text-start ${ activeSection === id ? 'active' : 'link-body-emphasis'}`}
+              className={`item section-parent ${ activeSection === id ? 'active' : ''}`}
               onClick={() => setActiveSection(id)}
             >
               {icon}
@@ -61,34 +64,25 @@ const Sidebar = () => {
         ))}
       </nav>
 
-      <div className="mt-auto">
+      <div className="user-panel">
         <LocaleSelectors />
-        <hr className="my-3" />
+        <hr />
         <UserDropdown user={user} logout={logout} />
       </div>
     </div>
   );
 };
 
-const SidebarItem = ({ item, onClick, onClose, activeSection }) => {
+const SidebarItem = ({ item, onClick, onClose, activeSection, isParent = false }) => {
   const isActive = activeSection === `${item.type}-${item.id}`;
+  const classPrefix = isParent ? 'parent' : 'children';
   
   return (
-    <div className={`d-flex align-items-center ps-3 ${isActive ? 'active' : ''}`}>
-      <a 
-        href="#"
-        className="flex-grow-1 text-truncate small"
-        onClick={(e) => { e.preventDefault(); onClick(); }}
-        title={item.name}
-      >
+    <div className={`item group-${classPrefix} ${isActive ? 'active' : ''}`}>
+      <button className={`group-${classPrefix}-name`} onClick={onClick} >
         {item.name}
-      </a>
-      <button 
-        className="btn btn-sm btn-link text-muted p-0 ms-2"
-        onClick={(e) => { e.stopPropagation(); onClose(); }}
-        title="Закрыть"
-        type="button"
-      >
+      </button>
+      <button className="item-close" onClick={onClose} title="Закрыть" >
         <i className="bi bi-x" />
       </button>
     </div>
@@ -101,7 +95,6 @@ const LocaleSelectors = () => (
       size="small"
       variant="borderless"
       defaultValue="ru"
-      // style={{ width: 80 }}
       suffixIcon=<ChevronDownIcon />
       options={[
         { value: 'ru', label: 'RU' },
@@ -112,7 +105,6 @@ const LocaleSelectors = () => (
       size="small"
       variant="borderless"
       defaultValue="USD"
-      // style={{ width: 80 }}
       suffixIcon=<ChevronDownIcon />
       options={[
         { value: 'USD', label: 'USD' },
@@ -174,17 +166,14 @@ const UserDropdown = ({ user, logout }) => {
 
 const SidebarHeader = () => (
   <>
-    <div className="d-flex gap-2 align-items-center">
-      <a 
-        href="/" 
-        className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
-      >
-        <img className="mb-0 me-2" src="/favicon.png" alt="Логотип" width="32" height="32" />
+    <div className="header-box">
+      <a href="/" className="home-link link-body-emphasis">
+        <img className="me-2" src="/favicon.png" alt="Логотип" width="32" height="32" />
         <span className="fs-4">Portfolios</span>
       </a>
-      <BellOutlined style={{ fontSize: '16px', color: '#6c757d', cursor: 'pointer' }} />
+      <BellOutlined style={{ color: '#6c757d', cursor: 'pointer' }} />
     </div>
-    <hr className="my-3" />
+    <hr />
   </>
 );
 
